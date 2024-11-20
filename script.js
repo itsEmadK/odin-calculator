3e485d502dec9f6233055c0e5ae975f5d9e71483
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-20 20:29:04 +0330

    Add operate function to choose between math functions

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ let calculator = {
     sub: (a, b) => a - b,
     multiply: (a, b) => a * b,
     division: (a, b) => a / b,
+    operate: (a, b, operator) => {
+        switch (operator) {
+            case "+":
+                return add(a, b);
+            case "-":
+                return sub(a, b);
+            case "*":
+                return multiply(a, b);
+            case "/":
+                return divide(a, b);
+            default:
+                break;
+        }
+    }
 }
