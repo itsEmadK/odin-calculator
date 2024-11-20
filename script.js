a4a7792b1122f731ec8f17249362da5771776387
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-20 23:28:55 +0330

    Map DOM to variables

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const DIGIT_BUTTON_SELECTOR = ".button.digit";
+const OPERATOR_BUTTON_SELECTOR = ".button.operator";
+const CALCULATOR_DISPLAY_SELECTOR = ".calculator-display";
+const CALCULATOR_MINI_DISPLAY_SELECTOR = ".calculator-mini-display";
+
 const calculator = new Calculator();
+
+const calcDisplay = document.querySelector(CALCULATOR_DISPLAY_SELECTOR);
+const calcMiniDisplay = document.querySelector(CALCULATOR_MINI_DISPLAY_SELECTOR);
+const digitButtons = document.querySelectorAll(DIGIT_BUTTON_SELECTOR);
+const operatorButtons = document.querySelectorAll(OPERATOR_BUTTON_SELECTOR);
 
 function Calculator() {
     this.add = (a, b) => a + b;
